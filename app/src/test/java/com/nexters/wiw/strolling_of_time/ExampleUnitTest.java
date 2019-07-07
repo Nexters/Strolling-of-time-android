26f0bccd8bdcf8c3df3c73c6a5e6b44ba3fd9bb3
Mincheol Kim <mcauto@naver.com>
2019-07-07 21:18:21 +0900

    test code to main

--- app/src/test/java/com/nexters/wiw/strolling_of_time/ExampleUnitTest.java
+++ app/src/test/java/com/nexters/wiw/strolling_of_time/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.nexters.wiw.strolling_of_time;
 
 import android.support.annotation.NonNull;
 
+import com.nexters.wiw.strolling_of_time.domain.PatternTimer;
+import com.nexters.wiw.strolling_of_time.models.EstimatedTime;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -22,24 +25,6 @@ import io.reactivex.disposables.Disposable;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-  class EstimatedTime{
-    private int time; // TimeUnit.SECONDS MINUTES HOURS
-    private TimeUnit unit; // 단위
-
-    EstimatedTime(int time, TimeUnit unit){
-      this.time = time;
-      this.unit = unit;
-    }
-
-    @NonNull
-    @Override
-    public String toString() {
-      return this.time + " " + this.unit.name();
-    }
-    public long toSeconds(){
-      return unit.toSeconds(this.time);
-    }
-  }
 
   class TimerMock extends Timer{
     private TimerTask task;
@@ -61,51 +46,6 @@ public class ExampleUnitTest {
     @Override
     public void run() {
       System.out.println("Task TEST!");
-    }
-  }
-
-  class PatternTimer implements Iterable<EstimatedTime>{
-    private TimerTask task;
-    private List<EstimatedTime> estimatedTimes;
-
-    PatternTimer(TimerTask task, List<EstimatedTime> estimatedTimes){
-      this.task = task;
-      this.estimatedTimes = estimatedTimes;
-    }
-
-    public void run(){
-      // TODO: Rxjava Completable.timer
-      // 1) 패턴 개수만큼 시작
-      for (EstimatedTime time : this) {
-        System.out.println(time.toString() + ": " + time.toSeconds());
-        // 2) 타이머 동작
-        this.task.run();
-      }
-    }
-
-    @NonNull
-    @Override
-    public Iterator<EstimatedTime> iterator() {
-      return new EstimatedIterator();
-    }
-
-    private class EstimatedIterator implements Iterator<EstimatedTime> {
-      private int position = 0;
-
-      public boolean hasNext() {
-        return position < estimatedTimes.size();
-      }
-
-      public EstimatedTime next() {
-        if (this.hasNext())
-          return estimatedTimes.get(position++);
-        else
-          return null;
-      }
-
-      @Override
-      public void remove() {
-      }
     }
   }
 

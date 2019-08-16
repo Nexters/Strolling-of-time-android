d44decb314406871586910a6e1a2d33c803da14c
김민철 <mcauto@naver.com>
2019-08-16 16:36:35 +0900

    modify toggle timer

--- app/src/main/java/com/nexters/wiw/strolling_of_time/views/timer/CerealTimer.java
+++ app/src/main/java/com/nexters/wiw/strolling_of_time/views/timer/CerealTimer.java
@@ -7,11 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import com.nexters.wiw.strolling_of_time.R;
 
 public class CerealTimer extends AppCompatActivity {
+    private boolean running = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,18 +33,33 @@ public class CerealTimer extends AppCompatActivity {
 
     private void initCerealTimer(){
         View cerealTimerBackground = findViewById(R.id.cereal_timer_background);
-        ToggleButton cerealTimerButton = findViewById(R.id.cereal_timer);
+        View cerealTimerButton = findViewById(R.id.cereal_timer);
         Button pauseButton = findViewById(R.id.cereal_timer_pause);
+
         TextView estimateLabel = findViewById(R.id.cereal_timer_estimate_time_label);
         TextView estimateTime = findViewById(R.id.cereal_timer_estimate_time);
 
-        cerealTimerButton.setOnCheckedChangeListener((compoundButton, running) -> {
+        TextView readyText = findViewById(R.id.tv_ready);
+
+        TextView estimateTimePercent = findViewById(R.id.tv_running_percent);
+        TextView totalRunningTime = findViewById(R.id.tv_total_running_time);
+        TextView percentMark = findViewById(R.id.percent_mark);
+
+        cerealTimerButton.setOnClickListener(v -> {
+
+            running = !running;
             int visibility = running ? View.VISIBLE : View.GONE;
             int backgroundColor = running ? R.color.cereal_timer_running_color_background : R.color.cereal_timer_ready_color_background;
             int resBackgroundTextColor = running ? R.color.cereal_timer_ready_color_background : R.color.cereal_timer_running_color_background;
             int backgroundTextColor = getResources().getColor(resBackgroundTextColor);
 
             pauseButton.setVisibility(visibility);
+            estimateTimePercent.setVisibility(visibility);
+            totalRunningTime.setVisibility(visibility);
+            percentMark.setVisibility(visibility);
+
+            readyText.setVisibility(running ? View.GONE : View.VISIBLE);
+
             cerealTimerBackground.setBackgroundResource(backgroundColor);
             estimateLabel.setTextColor(backgroundTextColor);
             estimateTime.setTextColor(backgroundTextColor);

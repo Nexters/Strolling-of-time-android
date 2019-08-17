ffa7003409be19d1ea96b95d9d2c50da5b2d9c24
Mincheol Kim <mcauto@naver.com>
2019-08-17 18:21:00 +0900

    ㅠㅠㅠㅠ 연결

--- app/src/main/java/com/nexters/wiw/strolling_of_time/views/group/GroupGenerateActivity.java
+++ app/src/main/java/com/nexters/wiw/strolling_of_time/views/group/GroupGenerateActivity.java
@@ -3,6 +3,7 @@ package com.nexters.wiw.strolling_of_time.views.group;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.nexters.wiw.strolling_of_time.R;
 
@@ -11,5 +12,12 @@ public class GroupGenerateActivity extends AppCompatActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_generate_group);
+    initToolbar();
+  }
+  private void initToolbar(){
+    TextView close_button = findViewById(R.id.close_button);
+    close_button.setOnClickListener(v->{
+      this.finish();
+    });
   }
 }
